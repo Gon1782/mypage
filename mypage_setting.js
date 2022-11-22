4c940345dbcd847a23e5a49da69494a361f0410e
Gon <hss3522@gmail.com>
2022-11-22 11:10:36 +0900

    backup

---
+++ mypage_setting.js
@@ -0,0 +1,19 @@
+function mod() {
+    info.style.visibility = 'hidden';
+    modify.style.visibility = 'visible';
+}
+
+function save() {
+    const urname = document.querySelector('#urnameinput').value;
+    const urintroduce = document.querySelector('#urintroduceinput').value;
+    const username = document.querySelector('#username');
+    const userintroduce = document.querySelector('#userintroduce');
+    if (urname === '') {
+        alert("이름을 입력해주세요.");
+        onclick.preventDefault();
+    }
+    username.innerHTML = urname;
+    userintroduce.innerHTML = urintroduce;
+    info.style.visibility = 'visible';
+    modify.style.visibility = 'hidden';
+}
